5702126bf5b49a081970947319f9663109d01125
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-04-30 16:10:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ read -p  "--> " N
 
 
 if [ "$N" -eq "1"  ]; then
- clear
- clear
  echo -e $g 'Please Wait   >-\n'
  sleep 0.4
  echo -e $g 'Please Wait   >--\n'
@@ -55,72 +53,7 @@ if [ "$N" -eq "1"  ]; then
  echo -e $g 'Please Wait   >[-----']\n'
  sleep 0.4
  clear
- echo -e $g 'Please Wait   >[------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait   >[-------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait   >[--------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait   >[---------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.  >[----------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait   >[-----------]\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.  >[------------]\n'
- sleep 0.2
- clear
- echo -e $g 'Please Wait   >[-------------]\n'
- sleep 0.2
- clear
- echo -e $g 'Please Wait.  >[--------------]\n'
- sleep 0.2
- clear
- echo -e $g 'Please Wait   >[---------------]\n'
- sleep 0.2
  apt install nodejs-lts
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait...\.\n'
- sleep 0.3
- clear
- echo -e $g 'Please Wait.../.\n'
- sleep 0.3
  clear
  echo -e $g 'Please Wait...\.\n'
  sleep 0.3
