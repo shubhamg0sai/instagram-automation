6d81d31731dee4865640aec2fdea921a81d9d97c
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-05 17:32:56 +0530

    Update Shubham.js

--- tools/Shubham.js
+++ tools/Shubham.js
@@ -109,12 +109,35 @@ const {
         `@${target} (User ID: ${id}) => Followers: ${info.follower_count}, Following: ${info.following_count}`,
         "ok"
       );
-    });
 
     print("Collecting followers . . .", "wait");
 
     let endOffLoop = false;
     do {
+      let items = await following.items();
+      items = _.chunk(items, perExec);
+      for (let i = 0; i < items.length; i++) {
+        await Promise.all(
+          items[i].map(async (user) => {
+            const unfollow = await ig.unfollow(user.pk);
+            print(
+              `▲ @${user.username} ⇶ ${
+                unfollow
+                  ? chalk.bold.green("Unfollowed!")
+                  : chalk.bold.red("Failed to Unfollow!")
+              }`
+            );
+          })
+        );
+        if (i < items.length - 1)
+          print(
+            `Current Account: (${login.username}) » Delay: ${perExec}/${delayTime}ms \n`,
+            "wait",
+            true
+          );
+        await delay(delayTime);
+      }
+
       const id = await ig.getIdByUsername(
         targetarray[Math.floor(Math.random() * targetarray.length)].trim("")
       );
@@ -177,58 +200,6 @@ const {
       await delay(delayTime);
       endOffLoop = targetFollowers.moreAvailable;
     } while (endOffLoop);
-    print(`Status: All Task done!`, "ok", true);
-  } catch (err) {
-    print(err, "err");
-  }
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const following = await ig.followingFeed();
-    print(
-      `Doing task with ratio ${perExec} target / ${delayTime} milliseconds \n`,
-      "wait"
-    );
-    do {
-      let items = await following.items();
-      items = _.chunk(items, perExec);
-      for (let i = 0; i < items.length; i++) {
-        await Promise.all(
-          items[i].map(async (user) => {
-            const unfollow = await ig.unfollow(user.pk);
-            print(
-              `▲ @${user.username} ⇶ ${
-                unfollow
-                  ? chalk.bold.green("Unfollowed!")
-                  : chalk.bold.red("Failed to Unfollow!")
-              }`
-            );
-          })
-        );
-        if (i < items.length - 1)
-          print(
-            `Current Account: (${login.username}) » Delay: ${perExec}/${delayTime}ms \n`,
-            "wait",
-            true
-          );
-        await delay(delayTime);
-      }
-    } while (following.moreAvailable);
     print(`Status: All Task done!`, "ok", true);
   } catch (err) {
     print(err, "err");
