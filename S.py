5fbff243bcc1e8d07d7a8b163d5eff814f6ffe4f
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-21 17:31:09 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,7 +1,11 @@
 import base64
-inp = int(input('''base64[1]\n base32[2]\n'''))
+import zlib
+
+
+inp = int(input('''base64[1]\nbase32[2]\nzlib[3]\nmarshal[4]\n'''))
+
 if inp == 1:
-    file = input("enter a file")
+    file = input("enter a file to encode into base64\n")
     f = open(file)
     sample_string = f.read()
     sample_string_bytes = sample_string.encode("ascii")
@@ -10,12 +14,27 @@ if inp == 1:
     print(f"Encoded string: {base64_string}")
 
 elif inp == 2:
-    file = input("enter a file")
+    file = input("enter a file to encode into base32\n")
     f = open(file)
     sample_string = f.read()
     sample_string_bytes = sample_string.encode("ascii")
-    base64_bytes = base64.b64encode(sample_string_bytes)
-    base64_string = base64_bytes.decode("ascii")
-    print(f"Encoded string: {base64_string}")
+    base32_bytes = base64.b32encode(sample_string_bytes)
+    base32_string = base32_bytes.decode("ascii")
+    print(f"Encoded string: {base32_string}")
+
+elif inp == 3:
+    file = input("enter a file to encode into zlib\n")
+    f = open(file)
+    sample_string = f.read()
+    zlib_string = zlib.compress(sample_string)
+    print(f"Encoded string: {zlib_string}")
+
+elif inp == 4:
+
+elif inp == 3:
+
+elif inp == 3:
+
+elif inp == 3:
 else:
     print("wrong input")
