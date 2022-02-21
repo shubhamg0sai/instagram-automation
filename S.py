974661d3f3fa2425876a1badcecb40c2470ef4bf
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-21 08:43:22 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,1 +1,15 @@
- f = open("test.txt")
+import base64 
+file = input("enter your file")
+f = open(file)
+sample_string = f
+sample_string_bytes = sample_string.encode("ascii") 
+
+  
+
+base64_bytes = base64.b64encode(sample_string_bytes) 
+
+base64_string = base64_bytes.decode("ascii") 
+
+  
+
+print(f"Encoded string: {base64_string}") 
