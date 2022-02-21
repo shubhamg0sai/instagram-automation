1ea7e86f2b8bc79476a7e7082c732239f76b4b0a
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-21 09:27:39 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,15 +1,21 @@
-import base64 
-file = input("enter your file")
-f = open(file)
-sample_string = f
-sample_string_bytes = sample_string.encode("ascii") 
+import base64
+inp = int(input('''base64[1]\n base32[2]\n'''))
+if inp == 1:
+    file = input("enter a file")
+    f = open(file)
+    sample_string = f.read()
+    sample_string_bytes = sample_string.encode("ascii")
+    base64_bytes = base64.b64encode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    print(f"Encoded string: {base64_string}")
 
-  
-
-base64_bytes = base64.b64encode(sample_string_bytes) 
-
-base64_string = base64_bytes.decode("ascii") 
-
-  
-
-print(f"Encoded string: {base64_string}") 
+elif inp == 2:
+    file = input("enter a file")
+    f = open(file)
+    sample_string = f.read()
+    sample_string_bytes = sample_string.encode("ascii")
+    base64_bytes = base64.b64encode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    print(f"Encoded string: {base64_string}")
+else:
+    print("wrong input")
